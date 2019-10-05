ac5e4f77de79571953a83d26212822f4baab4f28
Camila <camilasvqueiroz@gmail.com>
2019-10-05 12:08:37 -0300

    build teste redirects

---
+++ build/precache-manifest.d1ba15f906da40bbb4d837c65e12e2fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a6a58133eb90dc23cd0344b97b9cc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "709a91a8809b264c250e",
+    "url": "/static/js/2.607478c7.chunk.js"
+  },
+  {
+    "revision": "d112f83a5c5975afb14e",
+    "url": "/static/js/main.79868c25.chunk.js"
+  },
+  {
+    "revision": "097f5e5456befe3e0848",
+    "url": "/static/js/runtime~main.a60e1db1.js"
+  }
+]);
